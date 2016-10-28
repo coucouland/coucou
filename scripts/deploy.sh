265f338c999521f81d6f9e4bea3a08e54644d804
Ben Fortuna <benfortuna@gmail.com>
2016-10-28 04:01:26 +0000

    Updated bundle plugin

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-: ${SLING_HOST?"Need to set SLING_HOST variable"}
-: ${SLING_PORT?"Need to set SLING_PORT variable"}
-./gradlew build deployBundle -Psling_host=$SLING_HOST -Psling_port=$SLING_PORT \
-    && ./gradlew startBundle -Psling_host=$SLING_HOST -Psling_port=$SLING_PORT
