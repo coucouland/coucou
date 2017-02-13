41b31d68446032d19be2f93fc64c0a6b1be8b862
Ben Fortuna <benfortuna@gmail.com>
2017-02-13 04:30:49 +0000

    Enable card rendering with reactjs

--- coucou-app/src/main/resources/SLING-INF/content/js/components.js
+++ coucou-app/src/main/resources/SLING-INF/content/js/components.js
@@ -2,15 +2,13 @@
 
 function Jot(props) {
     return <div className="card">
-        <a href="/content/coucou/j/{props.jot_id}">
-            <div class="card-block">
-                <h4 class="card-title">{props.title}</h4>
-                <h6 class="card-subtitle text-muted">{props.summary}</h6>
-            </div>
-            <img src="http://lorempixel.com/g/150/100/business" alt="Sample image"/>
-            <div class="card-block">
-                <p class="card-text">{props.village}</p>
-            </div>
-        </a>
+        <div class="card-block">
+            <h4 class="card-title">{props.title}</h4>
+            <h6 class="card-subtitle text-muted">{props.summary}</h6>
+        </div>
+        <img src="http://lorempixel.com/g/150/100/business" alt="Sample image"/>
+        <div class="card-block">
+            <p class="card-text">{props.village}</p>
+        </div>
     </div>;
 }
